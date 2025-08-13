24874d0bc9b6d1d3974614f6bc51490465e62c87
Kumush-2010 <kumushizziyeva@gmail.com>
2025-08-13 15:34:25 +0500

    ..

--- api/server.js
+++ api/server.js
@@ -4,9 +4,7 @@ import { PORT } from './config/config.js'
 const app = express()
 import cors from 'cors'
 import { appRouter } from './router/router.js'
-import './bot/bot.js'
-
-
+      
 app.use(cors({
     origin: ['http://127.0.0.1:5500'],
     credentials: true
